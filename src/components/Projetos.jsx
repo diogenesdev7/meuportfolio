0f82bd09c1038af114f888416d0a04a422314663
Diógenes <123782296+diogenesdev7@users.noreply.github.com>
2023-12-11 15:50:32 -0300

    att projetos

--- src/components/Projetos.jsx
+++ src/components/Projetos.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import { DiBootstrap, DiGithub, DiCss3, DiHtml5, DiJavascript, DiPython, DiReact, DiSass } from 'react-icons/di'
+import { SiReact, SiSass, SiTailwindcss, SiTypescript  } from 'react-icons/si'
+import { BiLogoVuejs } from 'react-icons/bi'
 
 import '../styles/components/projetos.sass'
 import guitar from '../img/guitarclasses.jpg'
@@ -15,17 +17,19 @@ const Projetos = () => {
     <section className="projetos-container">
         <h2>Projetos</h2>
         <div className="projetos-cards">
-        <div className="projetos-card">
-            <div className="projetos-card-image">
-              <a href="https://queststore-wheat.vercel.app" target='_blank'><img src={quest} alt="quest store" /></a>
-            </div>
-            <div className="projetos-info-card">
-            <a href="https://queststore-wheat.vercel.app" target='_blank'><h4>Loja virtual de games</h4></a>
-              <p>Desenvolvido com React e Sass.</p>
-            </div>
-              <a className='git-link' href="https://github.com/diogenesdev7/queststore">
-                <FiExternalLink />
-              </a>
+          <div className="projetos-card">
+              <div className="projetos-card-image">
+                <a href="https://queststore-wheat.vercel.app" target='_blank'><img src={quest} alt="quest store" /></a>
+              </div>
+              <div className="projetos-info-card">
+              <a href="https://queststore-wheat.vercel.app" target='_blank'><h4>Loja virtual de games</h4></a>
+                <div className="tec">
+                  React e Sass
+                </div>
+              </div>
+                <a className='git-link' href="https://github.com/diogenesdev7/queststore">
+                  <FiExternalLink />
+                </a>
           </div>
 
           <div className="projetos-card">
@@ -34,7 +38,7 @@ const Projetos = () => {
             </div>
             <div className="projetos-info-card">
             <a href="https://diogenesdev7.github.io/html-css/guitar%20classes/" target='_blank'><h4>Landing Page de Aulas de Música</h4></a>
-              <p>Desenvolvido com Html e Sass</p>
+              <p>Html e Sass</p>
             </div>
               <a className='git-link' href="https://github.com/diogenesdev7/html-css/tree/main/guitar%20classes" target="_blank">
                 <FiExternalLink />
@@ -46,7 +50,7 @@ const Projetos = () => {
             </div>
             <div className="projetos-info-card">
             <a href="https://cinedatabase.vercel.app/" target='_blank'><h4>Site de Base de dados de filmes</h4></a>
-              <p>Desenvolvido com React.js, Sass e Api</p>
+              <p>React, Sass e Api</p>
             </div>
               <a className='git-link' href="https://github.com/diogenesdev7/cinedatabase" target="_blank">
                 <FiExternalLink />
@@ -58,7 +62,7 @@ const Projetos = () => {
             </div>
             <div className="projetos-info-card">
             <a href="https://diogenesdev7.github.io/todolist_react/" target='_blank'><h4>ToDo list</h4></a>
-              <p>Desenvolvido com React.js</p>
+              <p>React</p>
             </div>
               <a className='git-link' href="https://github.com/diogenesdev7/todolist_react" target="_blank">
                 <FiExternalLink />
@@ -70,7 +74,7 @@ const Projetos = () => {
             </div>
             <div className="projetos-info-card">
             <a href="https://diogenesdev7.github.io/html-css/casas_planejadas/" target='_blank'><h4>Site de planejamento de casas</h4></a>
-              <p>Desenvolvido com Bootstrap</p>
+              <p>Bootstrap</p>
             </div>
               <a className='git-link' href="https://github.com/diogenesdev7/html-css/tree/main/casas_planejadas" target="_blank">
                 <FiExternalLink />
@@ -82,21 +86,9 @@ const Projetos = () => {
             </div>
             <div className="projetos-info-card">
             <a href="https://diogenesdev7.github.io/previsao-do-tempo/" target='_blank'><h4>Sistema de previsão do tempo </h4></a>
-              <p>Desenvolvido com Vue.js</p>
+              <p>Vue</p>
             </div>
               <a className='git-link' href="https://github.com/diogenesdev7/previsao-do-tempo" target="_blank">
-                <FiExternalLink />
-              </a>
-          </div>
-          <div className="projetos-card">
-            <div className="projetos-card-image">
-            <a href="https://diogenesdev7.github.io/calculadoraIMC/" target='_blank'><img src={calculadoraIMC} alt="Calculadora IMC" /></a>
-            </div>
-            <div className="projetos-info-card">
-            <a href="https://diogenesdev7.github.io/calculadoraIMC/" target='_blank'><h4>Calculadora IMC </h4></a>
-              <p>Desenvolvido com Vue.js</p>
-            </div>
-              <a className='git-link' href="https://github.com/diogenesdev7/calculadoraIMC" target="_blank">
                 <FiExternalLink />
               </a>
           </div>
